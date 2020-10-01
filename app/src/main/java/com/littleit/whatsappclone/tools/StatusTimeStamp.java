705429402e08e00e25c7ea537b43b1e238dc89d0
caron <caronf82@gmail.com>
2020-10-01 21:30:54 +0530

    Initial commit

---
+++ app/src/main/java/com/littleit/whatsappclone/tools/StatusTimeStamp.java
@@ -0,0 +1,36 @@
+package com.littleit.whatsappclone.tools;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StatusTimeStamp {
+
+    public static void setUserStatus(String status) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null) {
+            String userID = firebaseUser.getUid();
+        }
+        Calendar c = Calendar.getInstance();
+        System.out.println("Current time => "+c.getTime());
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        String formattedDate = df.format(c.getTime());
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("status", status);
+        map.put("timestamp", formattedDate);
+        reference.child("LastSeen").child(firebaseUser.getUid()).updateChildren(map);
+
+
+    }
+}
---
+++ app/src/main/java/com/littleit/whatsappclone/view/MainActivity.java
@@ -0,0 +1,198 @@
+package com.littleit.whatsappclone.view;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.littleit.whatsappclone.R;
+import com.littleit.whatsappclone.databinding.ActivityMainBinding;
+import com.littleit.whatsappclone.menu.CallsFragment;
+import com.littleit.whatsappclone.menu.ChatsFragment;
+import com.littleit.whatsappclone.menu.StatusFragment;
+import com.littleit.whatsappclone.tools.StatusTimeStamp;
+import com.littleit.whatsappclone.view.activities.contact.ContactsActivity;
+import com.littleit.whatsappclone.view.activities.settings.SettingsActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    DatabaseReference reference;
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        setStatusOfflineOnDisconnect();
+        setUpWithViewPager(binding.viewPager);
+        binding.tabLayout.setupWithViewPager(binding.viewPager);
+        setSupportActionBar(binding.toolbar);
+
+
+        binding.fabAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ContactsActivity.class));
+            }
+        });
+
+        binding.viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                changeFabICon(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        StatusTimeStamp.setUserStatus("Online");
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        StatusTimeStamp.setUserStatus("Offline");
+    }
+
+    private void setUpWithViewPager(ViewPager viewPager){
+        MainActivity.SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ChatsFragment(),"Chats");
+        adapter.addFragment(new StatusFragment(),"Status");
+        adapter.addFragment(new CallsFragment(),"Calls");
+        viewPager.setAdapter(adapter);
+    }
+    //Add this code
+    private static class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public SectionsPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.menu_search :  Toast.makeText(MainActivity.this, "Action Search", Toast.LENGTH_LONG).show(); break;
+            case R.id.action_new_group :  Toast.makeText(MainActivity.this, "Action New Group", Toast.LENGTH_LONG).show(); break;
+            case R.id.action_new_broadcast :  Toast.makeText(MainActivity.this, "Action New Broadcast", Toast.LENGTH_LONG).show(); break;
+            case R.id.action_wa_web :  Toast.makeText(MainActivity.this, "Action Web", Toast.LENGTH_LONG).show(); break;
+            case R.id.action_starred_message :  Toast.makeText(MainActivity.this, "Action Starred Message", Toast.LENGTH_LONG).show(); break;
+            case R.id.action_settings :
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void changeFabICon(final int index){
+        binding.fabAction.hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                switch (index){
+                    case 0 : binding.fabAction.setImageDrawable(getDrawable(R.drawable.ic_chat_black_24dp));
+                                binding.fabAction.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        startActivity(new Intent(MainActivity.this, ContactsActivity.class));
+                                    }
+                                });
+
+                    break;
+                    case 1 : binding.fabAction.setImageDrawable(getDrawable(R.drawable.ic_camera_alt_black_24dp)); break;
+                    case 2 : binding.fabAction.setImageDrawable(getDrawable(R.drawable.ic_call_black_24dp)); break;
+                }
+                binding.fabAction.show();
+            }
+        },400);
+
+    }
+
+    private void setStatusOfflineOnDisconnect(){
+        //to set status offline on disconnect
+        reference=FirebaseDatabase.getInstance().getReference();
+        firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+        reference.child("LastSeen").child(firebaseUser.getUid()).child("status").onDisconnect().setValue("Offline");
+
+
+    }
+
+
+}
